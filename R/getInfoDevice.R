43f79241bbbf631665b0bb2155794f096557dab6
Devaucoup <43946233+TheoDEP@users.noreply.github.com>
2018-10-08 18:41:05 +0200

    Delete getInfoDevice.R

--- R/getInfoDevice.R
+++
@@ -1,9 +0,0 @@
-getInfoDevice <- function(token)
-{
-   return(
-      httr::GET(url = "https://data.qivivo.com/api/v2/devices",
-                httr::accept_json(),
-                httr::add_headers(Authorization = sprintf("Bearer %s",token$credentials$access_token)),
-                config = list(token = token))
-   )
-}
