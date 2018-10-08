6d14e363700bd06b6a6c43a2596111263840c89d
Devaucoup <43946233+TheoDEP@users.noreply.github.com>
2018-10-08 18:29:36 +0200

    oriented-object programming

--- R/connect.R
+++ R/connect.R
@@ -1,30 +1,92 @@
-#' Title
-#'
-#' @param appName TODO
-#' @param clientID TODO
-#' @param secretID TODO
-#' @param redirectURI TODO
-#'
-#' @return TODO
-#' @export
+methods::setOldClass("Token2.0")
+methods::setClass(Class = "rQivivo",
+                  representation = methods::representation(token = "Token2.0",
+                                                           UUID = "character")
+)
+
+#### Constructeur ####
 connect <- function(appName = "QivivoTheo",
                     clientID = "DMZEHQiwDg0UuhWNn1M7lCjjxoxmDqKxfZnl86rU",
                     secretID = "2UefAS8Qf9pFvKndoXXNHWZuoURfAe2lztm5vssJIMUnjlUBlL",
                     redirectURI = "http://127.0.0.1:1410")
 {
+   token <- httr::oauth2.0_token(endpoint = httr::oauth_endpoint(request = NULL,
+                                                                 authorize = "https://account.qivivo.com",
+                                                                 access = "https://account.qivivo.com/oauth/token"),
+                                 app = httr::oauth_app(appname = appName,
+                                                       key = clientID,
+                                                       secret = secretID,
+                                                       redirect_uri = redirectURI),
+                                 use_basic_auth = TRUE,
+                                 as_header = TRUE,
+                                 client_credentials = FALSE,
+                                 use_oob = FALSE,
+                                 cache = FALSE,
+                                 scope = paste(c("user_basic_information","read_devices","read_thermostats","read_wireless_modules",
+                                                 "read_programmation","update_programmation","read_house_data","update_house_settings"),
+                                               collapse = " "))# update_thermostat")
+
+   UUID <- httr::content(httr::GET(url = "https://data.qivivo.com/api/v2/devices",
+                                   httr::accept_json(),
+                                   httr::add_headers(Authorization = sprintf("%s %s",
+                                                                             object@token$credentials$token_type,
+                                                                             token$credentials$access_token)),
+                                   config = list(token = token)))$devices[[1]]$uuid
+
    return(
-      httr::oauth2.0_token(endpoint = httr::oauth_endpoint(request = NULL,
-                                                           authorize = "https://account.qivivo.com",
-                                                           access = "https://account.qivivo.com/oauth/token"),
-                           app = httr::oauth_app(appname = appName,
-                                                 key = clientID,
-                                                 secret = secretID,
-                                                 redirect_uri = redirectURI),
-                           use_basic_auth = TRUE,
-                           as_header = TRUE,
-                           client_credentials = FALSE,
-                           use_oob = FALSE,
-                           cache = FALSE,
-                           scope = "user_basic_information read_devices read_thermostats read_wireless_modules read_programmation update_programmation read_house_data update_house_settings")# update_thermostat")
+      methods::new(Class = "rQivivo", token = token, UUID = UUID)
    )
 }
+
+#### Impression ####
+methods::setMethod(f = "show",
+                   signature = "rQivivo",
+                   definition = function(object)
+                   {
+                      cat("An object of class \"rQvivio\"\n")
+                      cat("Authentification by OAuth 2.0 with httr package\n")
+                      print(object@token)
+                      cat("Thermostat UUID : ",object@UUID,"\n")
+                   })
+
+
+#### refresh token ####
+methods::setMethod(f = "refresh",
+                   signature = "rQivivo",
+                   definition = function(object)
+                   {
+                      objectName <- deparse(substitute(object))
+                      response <- httr::POST(url = "https://account.qivivo.com/oauth/token",
+                                             query = list(grant_type = "refresh_token",
+                                                          refresh_token = object@token$credentials$refresh_token,
+                                                          client_id = object@token$app$key))
+                      switch (EXPR = as.character(httr::status_code(response)),
+                              "200" = {
+                                 object@token$credentials <- httr::content(response)
+                                 assign(objectName, object, envir = parent.frame())
+                              },
+                              "401" = {
+                                 stop("Echec, veuillez recharger la page...")
+                              },
+                              stop("Echec, veuillez recharger la page..."))
+
+                      return(invisible())
+                   })
+
+#### get thermostat ####
+methods::setMethod(f = "getThermostat",
+                   signature = "rQivivo",
+                   definition = function(object, type)
+                   {
+                      if (all(type != c("info", "temperature", "humidity", "presence")) || length(type) != 1)
+                      {
+                         stop("L'argument \"type\" ne peut prendre comme valeur seulement \"info\", \"temperature\", \"humidity\" ou \"presence\"")
+                      }
+                      response <- httr::GET(url = sprintf("https://data.qivivo.com/api/v2/devices/thermostats/%s/%s", object@UUID, type),
+                                            httr::accept_json(),
+                                            httr::add_headers(Authorization = sprintf("%s %s",
+                                                                                      object@token$credentials$token_type,
+                                                                                      object@token$credentials$access_token)),
+                                            config = list(token = object@token))
+                      return(httr::content(response))
+                   })
---
+++ R/generics.R
@@ -0,0 +1,11 @@
+methods::setGeneric(name = "refresh",
+                    def = function(object)
+                    {
+                       standardGeneric("refresh")
+                    })
+
+methods::setGeneric(name = "getThermostat",
+                    def = function(object, type)
+                    {
+                       standardGeneric("getThermostat")
+                    })
